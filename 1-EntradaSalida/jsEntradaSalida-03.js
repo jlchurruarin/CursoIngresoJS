863e0b3bdd54b64e11bba768341bbf0222f89635
José Luis Churruarin <jlchurruarin@gmail.com>
2022-07-05 20:01:13 -0300

    Agregamos nombre al ej 3

--- 1-EntradaSalida/jsEntradaSalida-03.js
+++ 1-EntradaSalida/jsEntradaSalida-03.js
@@ -1,4 +1,5 @@
 /*
+José Luis Churruarin - División G
 Debemos lograr tomar un dato por 'ID'
 y luego mostrarlo por 'Alert' al presionar el botón  'mostrar'*/
 function mostrar()
@@ -7,7 +8,6 @@ function mostrar()
 	//nombreIngresado=txtIdNombre.value; Solo en chrome.
 	nombreIngresado=document.getElementById("txtIdNombre").value; // Funciona en cualquier navegador.
 	alert(nombreIngresado);
-
 }
 
 
