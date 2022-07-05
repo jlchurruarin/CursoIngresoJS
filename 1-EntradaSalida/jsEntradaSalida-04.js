38c705f065674ac6bee01fca4b8872f28650bbb7
José Luis Churruarin <jlchurruarin@gmail.com>
2022-07-05 20:10:16 -0300

    Ejercicio 4 resuelto

--- 1-EntradaSalida/jsEntradaSalida-04.js
+++ 1-EntradaSalida/jsEntradaSalida-04.js
@@ -1,9 +1,12 @@
 /* 
+José Luis Churruarin - División G
 Debemos lograr tomar un dato por 'PROMPT' 
 y lo muestro por 'ID' al presionar el botón 'mostrar'
 */
 function mostrar()
 {
-	alert("ok");
+	let textoIngresado;
+	textoIngresado = prompt("Ingrese un dato:");
+	document.getElementById("txtIdNombre").value= textoIngresado;
 }
 
