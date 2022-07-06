816b6368e6c64c46c805daff8b960b3750eeccb2
José Luis Churruarin <jlchurruarin@gmail.com>
2022-07-05 21:50:25 -0300

    Agregado nombre a ejercicio 7 y 8

--- 1-EntradaSalida/jsEntradaSalida-07.js
+++ 1-EntradaSalida/jsEntradaSalida-07.js
@@ -1,4 +1,5 @@
 /*
+José Luis Churruarin - División G
 Debemos lograr tomar Los numeros por ID ,
 transformarlos a enteros (parseInt),realizar la operación correcta y 
 mostrar el resulto por medio de "ALERT"
--- 1-EntradaSalida/jsEntradaSalida-08.js
+++ 1-EntradaSalida/jsEntradaSalida-08.js
@@ -1,4 +1,5 @@
 /*
+José Luis Churruarin - División G
 Debemos lograr tomar Los numeros por ID ,
 transformarlos a enteros (parseInt),realizar la operación correcta y 
 mostrar el resto entre el dividendo y el divisor.
