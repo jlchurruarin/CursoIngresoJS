e4fdbf0e7963af085896dd994da5ba525135c081
José Luis Churruarin <jlchurruarin@gmail.com>
2022-07-07 18:24:48 -0300

    TP 1 Resuelto

--- 8-TPs/01-jsFerreteFacturacion.js
+++ 8-TPs/01-jsFerreteFacturacion.js
@@ -1,17 +1,64 @@
 /*1.	Para el departamento de facturación:
+José Luis Churruarin
 A.	Ingresar tres precios de productos y mostrar la suma de los mismos.
 B.	Ingresar tres precios de productos y mostrar el promedio de los mismos.
 C.	ingresar tres precios de productos  y mostrar precio final (más IVA 21%).
 */
 function Sumar () 
 {
-	
+	let precioProductoUno;
+    let precioProductoDos;
+    let precioProductoTres;
+    let suma;
+
+    precioProductoUno = document.getElementById("txtIdPrecioUno").value;
+    precioProductoUno = parseFloat(precioProductoUno);
+    precioProductoDos = document.getElementById("txtIdPrecioDos").value;
+    precioProductoDos = parseFloat(precioProductoDos);
+    precioProductoTres = document.getElementById("txtIdPrecioTres").value;
+    precioProductoTres = parseFloat(precioProductoTres);
+
+    suma = precioProductoUno + precioProductoDos + precioProductoTres;
+
+    alert("La suma es: $" + suma);
+
 }
 function Promedio () 
 {
-	
+    let precioProductoUno;
+    let precioProductoDos;
+    let precioProductoTres;
+    let promedio;
+
+    precioProductoUno = document.getElementById("txtIdPrecioUno").value;
+    precioProductoUno = parseFloat(precioProductoUno);
+    precioProductoDos = document.getElementById("txtIdPrecioDos").value;
+    precioProductoDos = parseFloat(precioProductoDos);
+    precioProductoTres = document.getElementById("txtIdPrecioTres").value;
+    precioProductoTres = parseFloat(precioProductoTres);
+
+    promedio = (precioProductoUno + precioProductoDos + precioProductoTres) / 3;
+
+    alert("El promedio es: $" + promedio);
 }
 function PrecioFinal () 
 {
-	
+
+    let precioProductoUno;
+    let precioProductoDos;
+    let precioProductoTres;
+    let precioFinal;
+
+    precioProductoUno = document.getElementById("txtIdPrecioUno").value;
+    precioProductoUno = parseFloat(precioProductoUno);
+    precioProductoDos = document.getElementById("txtIdPrecioDos").value;
+    precioProductoDos = parseFloat(precioProductoDos);
+    precioProductoTres = document.getElementById("txtIdPrecioTres").value;
+    precioProductoTres = parseFloat(precioProductoTres);
+
+    precioFinal = precioProductoUno + precioProductoDos + precioProductoTres;
+    precioFinal = precioFinal * 1.21;
+
+    alert("El precio final es: $" + precioFinal);
+
 }
